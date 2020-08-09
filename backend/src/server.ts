00e54380a0e31022e8bec8dd5eb5574117f15d74
Vinicius Flores <viniciusflores379@gmail.com>
2020-08-09 00:26:18 +0000

    :tada: Started backend

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res)=>{
+  res.json({message: 'hello vini'})
+})
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!')
+})
