ab929403462daeaa9af954b95603e1d4361f5e88
Vinicius Flores <viniciusflores379@gmail.com>
2020-08-15 20:14:41 +0000

    :sparkles: include forgot password page

---
+++ frontend/src/pages/ForgotPassword/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from './styles'
+
+const ForgotPassword: React.FC = () => {
+  return (
+    <Container>
+      <h1>Forgot Password Page</h1>
+    </Container>
+  )
+}
+
+export default ForgotPassword
