df9e0f01e07f9d99157ddc79dbf2b9c4dd85fd6c
Vinicius Flores <viniciusflores379@gmail.com>
2020-08-15 19:36:22 +0000

    :construction: start layout signin page

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import { Container } from './styles'
+import React, { useCallback, useRef } from 'react'
+import { FiMail, FiLock } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+import { FormHandles } from '@unform/core'
+import { Form } from '@unform/web'
+
+import logoImg from '../../assets/laranjo.png'
+import Button from '../../components/ButtonSign'
+import Input from '../../components/InputSign'
+import { Container, Content } from './styles'
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null)
+  const handleSubmit = useCallback(() => {
+    alert('fez o submit')
+  }, [])
+
   return (
     <Container>
-      <h1>SignIn Page</h1>
+      <Content>
+        <img src={logoImg} alt="Aware" />
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Log In to Aware</h1>
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" type="email" />
+          <Input
+            name="password"
+            icon={FiLock}
+            placeholder="Password"
+            type="password"
+          />
+
+          <Button type="submit" dataTestId="btn-entrar">
+            Log In
+          </Button>
+
+          <Link to="/forgot-password" data-cy="link-to-forget-password">
+            Forgot password? °
+          </Link>
+
+          <Link to="/signup" data-cy="link-to-signup">
+            Sign up for Aware
+          </Link>
+        </Form>
+      </Content>
     </Container>
   )
 }
--- frontend/src/pages/SignIn/styles.ts
+++ frontend/src/pages/SignIn/styles.ts
@@ -1,3 +1,58 @@
 import styled from 'styled-components'
 
 export const Container = styled.div``
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+
+  img {
+    width: 80px;
+    height: 80px;
+    margin-top: 50px;
+  }
+
+  form {
+    text-align: center;
+    margin-top: 20px;
+    margin-bottom: 20px;
+
+    h1 {
+      font-weight: 600;
+    }
+
+    button {
+      margin-bottom: 10px;
+    }
+
+    a {
+      color: #312e38;
+      text-decoration: none;
+
+      & + a {
+        margin-left: 5px;
+      }
+    }
+  }
+`
+
+/*
+export const DEVICE_SIZE = {
+  MOBILE_S: 320,
+  MOBILE_M: 375,
+  MOBILE_L: 425,
+  TABLET: 768,
+  LAPTOP: 1024,
+  LAPTOP_L: 1440,
+  DESKTOP: 2560,
+};
+
+export const DEVICE_HEIGHT = {
+  MOBILE_S: '540px',
+};
+*/
