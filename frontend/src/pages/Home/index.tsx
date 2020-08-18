61002595a290de8d39b550f1d5c0d66d88a42a3a
Vinicius Flores <viniciusflores379@gmail.com>
2020-08-18 21:27:18 +0000

    :start post

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,36 +1,93 @@
 import React from 'react'
-import { Container, Header, Content, Footer } from './styles'
+import {
+  FiUser,
+  FiFeather,
+  FiHome,
+  FiSearch,
+  FiBell,
+  FiMail,
+  FiTwitter,
+} from 'react-icons/fi'
+import logoImg from '../../assets/laranjo.png'
+import { Container, Header, Content, Footer, NewTweet } from './styles'
 
 const Home: React.FC = () => {
   return (
     <Container>
       <Header>
+        <FiUser size={20} />
         <h1>Home</h1>
+        <FiFeather size={20} />
       </Header>
       <Content>
-        <p>twiit</p>
-        <p>twiit</p>
-        <p>twiit</p>
-        <p>twiit</p>
-        <p>twiit</p>
-        <p>twiit</p>
-        <p>twiit</p>
-        <p>twiit</p>
-        <p>twiit</p>
-        <p>twiit</p>
-        <p>twiit</p>
-        <p>twiit</p>
-        <p>twiit</p>
-        <p>twiit</p>
-        <p>twiit</p>
-        <p>twiit</p>
-        <p>twiit</p>
-        <p>twiit</p>
-        <p>twiit</p>
-        <p>twiit</p>
+        <img src={logoImg} alt="Lorem Ipsum" />
+        <div>
+          <span>
+            <p>Lorem Ipsum</p>
+            @lorem_ipsum ° 5m
+          </span>
+          <span>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum
+          </span>
+        </div>
       </Content>
+      {/* <Content>
+        <img src={logoImg} alt="Lorem Ipsum" />
+        <div>
+          <span>
+            <p>Lorem Ipsum</p>
+            @lorem_ipsum ° 5m
+          </span>
+          <span>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s,
+          </span>
+        </div>
+      </Content>
+      <Content>
+        <img src={logoImg} alt="Lorem Ipsum" />
+        <div>
+          <span>
+            <p>Lorem Ipsum</p>
+            @lorem_ipsum ° 5m
+          </span>
+          <span>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem IpsumLorem Ipsum is simply
+            dummy text of the printing and typesetting industry. Lorem Ipsum has
+            been the industry's standard dummy text ever since the 1500s, when
+            an unknown printer took a galley of type and scrambled it to make a
+            type specimen book. It has survived not only five centuries, but
+            also the leap into electronic typesetting, remaining essentially
+            unchanged. It was popularised in the 1960s with the release of
+            Letraset sheets containing Lorem Ipsum passages, and more recently
+            with desktop publishing software like Aldus PageMaker including
+            versions of Lorem Ipsum
+          </span>
+        </div>
+      </Content> */}
+
       <Footer>
-        <p>botao</p>
+        <FiHome size={20} />
+        <FiSearch size={20} />
+        <FiBell size={20} />
+        <FiMail size={20} />
       </Footer>
     </Container>
   )
--- frontend/src/pages/Home/styles.ts
+++ frontend/src/pages/Home/styles.ts
@@ -1,18 +1,64 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`
 
 export const Header = styled.header`
-  position: sticky;
+  display: flex;
   top: 0;
+  border-bottom: 2px solid gray;
+  flex-direction: row;
+  justify-content: space-between;
+  align-content: center;
+  align-items: center;
+
+  svg {
+    margin: 0 20px;
+  }
 `
 
 export const Content = styled.div`
-  margin: 0 auto;
-  line-height: 80px;
+  display: flex;
+  flex-direction: row;
+  margin-top: 10px;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    margin-left: 5px;
+
+    span {
+      display: flex;
+      flex-direction: row;
+
+      p {
+        font-weight: bold;
+        margin-right: 5px;
+        font-size: 18px;
+      }
+    }
+  }
+
+  img {
+    width: 40px;
+    height: 40px;
+  }
 `
 
 export const Footer = styled.footer`
-  position: sticky;
+  display: flex;
+  flex-direction: row;
   bottom: 0;
+  justify-content: space-around;
+  border-top: 2px solid gray;
+
+  svg {
+    margin-top: 10px;
+  }
+`
+export const NewTweet = styled.button`
+  width: 56px;
+  height: 56px;
 `
