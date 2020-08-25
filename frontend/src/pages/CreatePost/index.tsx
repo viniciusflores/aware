4b4642162327aabbe62d08e7133b9c1aa64ab248
Vinicius Flores <viniciusflores379@gmail.com>
2020-08-25 23:38:43 +0000

    :construction: start get content post

--- frontend/src/pages/CreatePost/index.tsx
+++ frontend/src/pages/CreatePost/index.tsx
@@ -1,12 +1,76 @@
-import React from 'react'
-import { Container } from './styles'
+import React, { useState, useEffect, useCallback, useRef } from 'react'
+import { Link } from 'react-router-dom'
+import { FiGlobe, FiArrowLeft } from 'react-icons/fi'
+import logoImg from '../../assets/laranjo.png'
+import { Container, Header, Content } from './styles'
 
-const CreatePost: React.FC = () => {
+const Home: React.FC = () => {
+  const [locationPermission, setLocationPermission] = useState(false)
+  const [accuracy, setAccuracy] = useState(0)
+  const [latitude, setLatitude] = useState(0)
+  const [longitude, setLongitude] = useState(0)
+  const [postContent, setPostContent] = useState('')
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        setLocationPermission(true)
+        setAccuracy(position.coords.accuracy)
+        setLatitude(position.coords.latitude)
+        setLongitude(position.coords.longitude)
+      },
+      function showError(error) {
+        setLocationPermission(false)
+        switch (error.code) {
+          case error.PERMISSION_DENIED:
+            console.log('User denied the request for Geolocation.')
+            break
+          case error.POSITION_UNAVAILABLE:
+            console.log('Location information is unavailable.')
+            break
+          case error.TIMEOUT:
+            console.log('The request to get user location timed out.')
+            break
+          default:
+            console.log('The request to get user location error default')
+            break
+        }
+      },
+    )
+  }, [])
+
+  const handlePostAction = useCallback(() => {
+    console.log(postContent)
+  }, [postContent])
+
+  const handlePostContent = useCallback(e => {
+    const abc = e.target.value
+    setPostContent(abc)
+  }, [])
+
   return (
     <Container>
-      <h1>Create Post Page</h1>
+      <Header>
+        <Link to="/">
+          <FiArrowLeft size={20} />
+        </Link>
+
+        <button type="button" onClick={handlePostAction}>
+          Post
+        </button>
+      </Header>
+      <Content>
+        <img src={logoImg} alt="Lorem Ipsum" />
+        <div className="content">
+          <input placeholder="What's happening?" onChange={handlePostContent} />
+          <div>
+            <FiGlobe />
+            Everyone can reply
+          </div>
+        </div>
+      </Content>
     </Container>
   )
 }
 
-export default CreatePost
+export default Home
--- frontend/src/pages/CreatePost/styles.ts
+++ frontend/src/pages/CreatePost/styles.ts
@@ -1,3 +1,66 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Header = styled.header`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.25);
+
+  a {
+    text-decoration: none;
+    color: #ff9000;
+    svg {
+      margin: 0 20px;
+      height: 25px;
+      width: 25px;
+    }
+  }
+
+  button {
+    margin: 0 15px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #ff9000;
+    width: 60px;
+    height: 24px;
+    border-radius: 16px;
+    border: 0;
+    color: #fff;
+    font-weight: bold;
+    font-size: 14px;
+  }
+`
+
+export const Content = styled.div`
+  display: flex;
+  margin: 10px 10px 0;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.25);
+  width: 100%;
+  /* position: relative;
+  min-height: 100%;
+  flex: 0; */
+  img {
+    width: 48px;
+    height: 48px;
+    margin-right: 10px;
+  }
+
+  div.content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    div {
+      display: flex;
+      align-items: center;
+      box-shadow: 0 1px 1px rgba(0, 0, 0, 0.25);
+    }
+  }
+`
