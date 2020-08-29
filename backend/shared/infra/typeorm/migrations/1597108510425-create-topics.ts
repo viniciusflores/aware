e2f38ba8652eec73906557a310c56ee016d753a0
Vinicius Flores <viniciusflores379@gmail.com>
2020-08-29 21:23:52 +0000

    :bug: fix timeout token

---
+++ backend/shared/infra/typeorm/migrations/1597108510425-create-topics.ts
@@ -0,0 +1,64 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export default class createPosts1597108510425 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'posts',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'content',
+            type: 'varchar',
+          },
+          {
+            name: 'visibility',
+            type: 'varchar',
+          },
+          {
+            name: 'latitude',
+            type: 'float',
+          },
+          {
+            name: 'longitude',
+            type: 'float',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'PostUser',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['user_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('posts')
+  }
+}
